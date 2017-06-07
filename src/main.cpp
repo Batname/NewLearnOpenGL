1d3754381e1d8f2f665179e4be406c1b44e6d2e3
BatName <dadubinin@gmail.com>
2017-06-07 22:45:55 +0200

    add color attribs

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,11 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 float vertices[] = {
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left
+    // Position          // color
+     0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // top right
+     0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // bottom left
+    -0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f  // top left
 };
 
 unsigned int indices[] = {
@@ -70,9 +71,12 @@ int main(int argc, char *argv[])
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Set the vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0); // Bind to location 0 inside shaders
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // Color arrtibute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // Reset buffers
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -89,13 +93,7 @@ int main(int argc, char *argv[])
         // Activate shader
         defaultShader.use();
 
-
-        // Update uniform color
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue) / 2.0f +.05f;
-        int vertexColorLocation = glGetUniformLocation(defaultShader.ID, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
+        // Draw triangles
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
